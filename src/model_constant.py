f280d4e6e741ebfa5d10618b4ba32aa2d7c36697
MFrancys <maria.lanza@truehome.com.mx>
2021-05-31 19:43:20 -0500

    review

--- src/model_constant.py
+++ src/model_constant.py
@@ -1,24 +1,95 @@
 import numpy as np
 
-from sklearn.pipeline import Pipeline
-from sksurv.ensemble import RandomSurvivalForest
+regressor_config_dict = {
 
-pipeline = Pipeline([
-#    ("reduction", PCA(n_components=0.95)),
-    ("random_survival_forest", RandomSurvivalForest(random_state=123))
-])
+    'sklearn.linear_model.ElasticNetCV': {
+        'l1_ratio': np.arange(0.0, 1.01, 0.05),
+        'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    },
 
-GRIDSEARCH_PARAMETERS = {
-    #"n_estimators":
-    #    [int(x) for x in np.linspace(start=200, stop=2000, num=100)], # Number of trees in random forest
-    #"max_features":
-    #    ["auto", "sqrt", "log2"], # Number of features to consider at every split
-    #"max_depth":
-    #    [int(x) for x in np.linspace(10, 110, num=11)], # Maximum number of levels in tree
-    "min_samples_split":
-        [2, 5, 10], # Minimum number of samples required to split a node
-    "min_samples_leaf":
-        [1, 2, 4], # Minimum number of samples required at each leaf node
-    "bootstrap":
-        [True, False] # Method of selecting samples for training each tree
+    'sklearn.ensemble.ExtraTreesRegressor': {
+        'n_estimators': [100],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'bootstrap': [True, False]
+    },
+
+    'sklearn.ensemble.GradientBoostingRegressor': {
+        'n_estimators': [100],
+        'loss': ["ls", "lad", "huber", "quantile"],
+        'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
+        'max_depth': range(1, 11),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'subsample': np.arange(0.05, 1.01, 0.05),
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'alpha': [0.75, 0.8, 0.85, 0.9, 0.95, 0.99]
+    },
+
+    'sklearn.ensemble.AdaBoostRegressor': {
+        'n_estimators': [100],
+        'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
+        'loss': ["linear", "square", "exponential"]
+    },
+
+    'sklearn.tree.DecisionTreeRegressor': {
+        'max_depth': range(1, 11),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21)
+    },
+
+    'sklearn.neighbors.KNeighborsRegressor': {
+        'n_neighbors': range(1, 101),
+        'weights': ["uniform", "distance"],
+        'p': [1, 2]
+    },
+
+    'sklearn.linear_model.LassoLarsCV': {
+        'normalize': [True, False]
+    },
+
+    'sklearn.svm.LinearSVR': {
+        'loss': ["epsilon_insensitive", "squared_epsilon_insensitive"],
+        'dual': [True, False],
+        'tol': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1],
+        'C': [1e-4, 1e-3, 1e-2, 1e-1, 0.5, 1., 5., 10., 15., 20., 25.],
+        'epsilon': [1e-4, 1e-3, 1e-2, 1e-1, 1.]
+    },
+
+    'sklearn.ensemble.RandomForestRegressor': {
+        'n_estimators': [100],
+        'max_features': np.arange(0.05, 1.01, 0.05),
+        'min_samples_split': range(2, 21),
+        'min_samples_leaf': range(1, 21),
+        'bootstrap': [True, False]
+    },
+
+    'sklearn.linear_model.RidgeCV': {
+    },
+
+    'xgboost.XGBRegressor': {
+        'n_estimators': [100],
+        'max_depth': range(1, 11),
+        'learning_rate': [1e-3, 1e-2, 1e-1, 0.5, 1.],
+        'subsample': np.arange(0.05, 1.01, 0.05),
+        'min_child_weight': range(1, 21),
+        'n_jobs': [1],
+        'verbosity': [0],
+        'objective': ['reg:squarederror']
+    },
+
+    'sklearn.linear_model.SGDRegressor': {
+        'loss': ['squared_loss', 'huber', 'epsilon_insensitive'],
+        'penalty': ['elasticnet'],
+        'alpha': [0.0, 0.01, 0.001] ,
+        'learning_rate': ['invscaling', 'constant'] ,
+        'fit_intercept': [True, False],
+        'l1_ratio': [0.25, 0.0, 1.0, 0.75, 0.5],
+        'eta0': [0.1, 1.0, 0.01],
+        'power_t': [0.5, 0.0, 1.0, 0.1, 100.0, 10.0, 50.0]
+    }
+
 }
+
+#Source: https://github.com/EpistasisLab/tpot/blob/master/tpot/config/regressor.py
